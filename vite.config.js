a9b568a18c2b8f2b0a23a2348b1ab2066ebe6380
Haochuan Zhou <mrzhouhc@icloud.com>
2025-09-30 12:44:24 +1000

    add the base repo name for vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const repoName = "/3d-snake-game/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoName,
   plugins: [react(), tailwindcss()],
 });
